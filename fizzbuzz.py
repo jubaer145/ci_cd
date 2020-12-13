f4f2bc3fa9397e6166ca3eac0cf86d877bef311b
jubaer145 <mdjubaerhossain7@gmail.com>
2020-12-13 12:58:49 +0600

    improved fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,9 +5,17 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-## solution starts here
-def fizz_buzz(n : int) -> str:
-    in n % 15 == 0: return "fizz_buzz"
-    elif n % 5 == 0: return "buzz"
-    elif n % 3 == 0: return "fizz"
-    else: return str(n)
+"""Function to solve the fizzbuzz problem."""
+def fizz_buzz(num: int) -> str:
+    """This is my great and neat function to solve the famous
+    Fizz Buzz problem.
+    :param num: That's the number which we want the answer for
+    :return: fizz, buzz, fizzbuzz or the number itself
+    """
+    if num % 15 == 0:
+        return "fizzbuzz"
+    if num % 5 == 0:
+        return "buzz"
+    if num % 3 == 0:
+        return "fizz"
+    return str(num)
